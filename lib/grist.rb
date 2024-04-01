17a00e0cd1e0db624b82e242ed887e3d48294145
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-04-01 12:44:53 +0200

    feat: initial commit

---
+++ lib/grist.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "grist/version"
+
+module Grist
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/grist_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Grist do
+  it "has a version number" do
+    expect(Grist::VERSION).to eq("0.1.0")
+  end
+end
