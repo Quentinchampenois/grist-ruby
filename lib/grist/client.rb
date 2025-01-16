8c596112a0e3c8d853dea94c149283bd359c0db3
Quentin Champenois <quentin.champenois7@gmail.com>
2025-01-16 22:31:57 +0100

    fix(client): Update client

--- lib/grist/client.rb
+++ lib/grist/client.rb
@@ -1,14 +1,30 @@
 module Grist
   class Client
-    attr_accessor :base_url, :api_key
+    attr_reader :base_url, :api_key
 
     BASE_URL = "https://api.getgrist.com"
 
-    def initialize(url: nil, api_key: nil)
+    def initialize(api_key:, url:)
       @base_url = url || BASE_URL
-      raise MissingApiKey if api_key.nil?
-
       @api_key = api_key
+    end
+
+    def request(method, endpoint, params = {})
+      uri = URI("#{base_url}/#{endpoint}")
+      uri.query = URI.encode_www_form(params) if method == :get
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::const_get(method.capitalize).new(uri)
+      request["Authorization"] = "Bearer #{api_key}"
+      request["Content-Type"] = "application/json"
+      request.body = params.to_json unless method == :get
+
+      response = http.request(request)
+      JSON.parse(response.body)
+    rescue StandardError => e
+      { error: e.message }
     end
   end
 end
--- spec/lib/grist/client_spec.rb
+++ spec/lib/grist/client_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe Grist::Client do
-  subject { described_class.new(url: base_url, api_key: api_key) }
+  subject { described_class.new(api_key: api_key, url: base_url) }
   let(:base_url) { "https://api.example.com" }
   let(:api_key) { "1234" }
 
@@ -13,16 +13,9 @@ RSpec.describe Grist::Client do
 
     context "when base_url is not defined" do
       let(:base_url) { nil }
+
       it "initializes with a default base_url" do
         expect(subject.base_url).to eq("https://api.getgrist.com")
-      end
-    end
-
-    context "when api_key is not defined" do
-      let(:api_key) { nil }
-
-      it "raises an MissingApiKey error" do
-        expect { subject.api_key }.to raise_error(Grist::MissingApiKey)
       end
     end
   end
